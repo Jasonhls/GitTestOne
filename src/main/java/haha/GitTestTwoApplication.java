3d30c0dad8096e7258a0010e7902c19fd3eef7cb
Jasonhls <1597445096@qq.com>
2018-07-22 14:09:37 +0800

    first commit

---
+++ src/main/java/haha/GitTestTwoApplication.java
@@ -0,0 +1,12 @@
+package haha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestTwoApplication.class, args);
+	}
+}
